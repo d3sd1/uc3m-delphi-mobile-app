49a7fb1512a7f5d602bdcce798199bbea1cd1040
Andrei García <andreigarciacuadra@gmail.com>
2020-11-02 15:31:06 +0100

    tested few components from ionic

--- src/app/tab1/proccesses/proccesses.component.ts
+++ src/app/tab1/proccesses/proccesses.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {IonInfiniteScroll, NavController} from '@ionic/angular';
-import {FingerprintAIO, FingerprintOptions} from '@ionic-native/fingerprint-aio/ngx';
+import {Component, OnInit} from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-proccesses',
@@ -8,29 +7,38 @@ import {FingerprintAIO, FingerprintOptions} from '@ionic-native/fingerprint-aio/
   styleUrls: ['./proccesses.component.scss'],
 })
 export class ProccessesComponent implements OnInit {
-  fingerprintOptions: FingerprintOptions;
 
-  constructor(public navCtrl: NavController, private fingerAuth: FingerprintAIO) {
+  coords = 0;
+  constructor(private geolocation: Geolocation) {}
 
-  }
-
-  ngOnInit(): void {
-  }
-
-  public showFingerprintAuthDlg() {
-    this.fingerprintOptions = {
-      // @ts-ignore
-      clientId: 'fingerprint-Demo',
-      clientSecret: 'password',
-      disableBackup: true,
-    };
-    this.fingerAuth.isAvailable().then(result => {
-      if (result === 'OK') {
-        this.fingerAuth.show(this.fingerprintOptions)
-          // tslint:disable-next-line:no-shadowed-variable
-          .then((result: any) => console.log(result))
-          .catch((error: any) => console.log(error));
-      }
+  ngOnInit() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      this.coords = resp.coords.latitude;
+      console.log("LOOONG " + resp.coords.longitude)
+    }).catch((error) => {
+      console.log('Error getting location', error);
     });
+
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    });
+    /*
+        this.faio.show({
+          title: 'Biometric Authentication', // (Android Only) | optional | Default: "<APP_NAME> Biometric Sign On"
+          subtitle: 'Coolest Plugin ever' // (Android Only) | optional | Default: null
+          description: 'Please authenticate' // optional | Default: null
+          fallbackButtonTitle: 'Use Backup', // optional | When disableBackup is false defaults to "Use Pin".
+                                             // When disableBackup is true defaults to "Cancel"
+          disableBackup:true,  // optional | default: false
+        })
+          .then((result: any) => console.log(result))
+          .catch((error: any) => console.log(error));*/
+
   }
+
 }
