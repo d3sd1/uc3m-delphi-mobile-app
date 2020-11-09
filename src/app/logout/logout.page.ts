c9c67e425f93b68f50f259eab008f2bc0e05f233
Andrei García <andreigarciacuadra@gmail.com>
2020-11-09 19:26:05 +0100

    DEL-6 #todo #comment Missing files.

---
+++ src/app/logout/logout.page.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../services/authentication-service';
+import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.page.html',
+  styleUrls: ['./logout.page.scss'],
+})
+export class LogoutPage implements OnInit {
+
+  constructor(private authenticationService: AuthenticationService, private router: Router, private toastController: ToastController) {
+  }
+
+
+  async sendToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    await toast.present();
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.authenticationService.logout().then(() => {
+        this.router.navigateByUrl('login').then(() => {
+          this.sendToast('Desconexión satisfactoria').then();
+        });
+      }).catch(() => {
+
+      });
+    }, 2000);
+  }
+
+}
