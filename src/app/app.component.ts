be8576ce3128ec80c1f853d00ee5b6d4bf12c27f
Andrei García <andreigarciacuadra@gmail.com>
2020-11-13 16:46:04 +0100

    DEL-22 #done #comment Added support for dark mode devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,19 @@ export class AppComponent {
     private initApp: InitService,
     private loadingController: LoadingController
   ) {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+    // Add or remove the "dark" class based on if the media query matches
+    function toggleDarkTheme(shouldAdd) {
+      document.body.classList.toggle('dark', shouldAdd);
+    }
+
     this.initializeApp();
   }
 
@@ -38,7 +51,6 @@ export class AppComponent {
       duration: 2000
     });
     await this.loading.present();
-
   }
 
   initializeApp() {
