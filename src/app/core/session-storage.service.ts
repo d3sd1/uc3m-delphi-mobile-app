ed12eb275f5e7f7c2a5704df7cd53509f98f31d7
Andrei García <andreigarciacuadra@gmail.com>
2021-01-31 01:10:00 +0100

    DH-19 #done #comment added session-storage.service.ts to project

---
+++ src/app/core/session-storage.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Storage} from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionStorageService {
+  private USER_STORAGE_KEY = 'USER_STRG';
+  private userUpdater: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+
+  constructor(private  storage: Storage) {
+    this.fetchLocalStorageUser().then((user: User) => {
+      this.userUpdater.next(user);
+    });
+  }
+
+  store(user: User, jwt: string): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.storage.set(this.USER_STORAGE_KEY, JSON.stringify(user));
+        this.userUpdater.next(user);
+        resolve(true);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  fetch(): BehaviorSubject<User> {
+    return this.userUpdater;
+  }
+
+  private async fetchLocalStorageUser(): Promise<User> {
+    return JSON.parse(await this.storage.get(this.USER_STORAGE_KEY));
+  }
+}
