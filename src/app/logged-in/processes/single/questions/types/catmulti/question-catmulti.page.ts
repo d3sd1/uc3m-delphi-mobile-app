cbfe50d9d27d748af89ae2ce487d46cd5df7da5d
Andrei García <andreigarciacuadra@gmail.com>
2021-06-07 08:48:42 +0200

    DEL-48 all kinds of questions working

--- src/app/logged-in/processes/single/questions/types/catmulti/question-catmulti.page.ts
+++ src/app/logged-in/processes/single/questions/types/catmulti/question-catmulti.page.ts
@@ -1,27 +1,82 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Question} from '../../../../../../core/model/question';
 import {Category} from '../../../../../../core/model/category';
 import {Process} from '../../../../../../core/model/process';
+import {ToastController} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../../../../../environments/environment';
 
 @Component({
   selector: 'delphi-question-catmulti',
   templateUrl: './question-catmulti.page.html',
   styleUrls: ['./question-catmulti.page.scss'],
 })
-export class QuestionCatmultiPage {
+export class QuestionCatmultiPage implements OnInit{
   @Input()
   questionIdx: number;
   @Input()
   process: Process;
   currentCategory = '';
-  responseRange = 0;
-  addCategory() {
-    this.process.currentRound[this.questionIdx].categories.push(new Category(this.currentCategory));
-    this.currentCategory = '';
+
+
+  constructor(private toastController: ToastController, private httpClient: HttpClient) {
   }
-  delCategory(category: Category) {
-    this.process.currentRound[this.questionIdx].categories = this.process.currentRound[this.questionIdx].categories.filter((cat) => {
+
+  ngOnInit(): void {
+    this.reorderCategories();
+  }
+
+
+  async addCategory() {
+    if(this.process.currentRound.questions[this.questionIdx].categories === undefined ||
+      this.process.currentRound.questions[this.questionIdx].categories === null) {
+      this.process.currentRound.questions[this.questionIdx].categories = [];
+    }
+
+    if(this.currentCategory === '') {
+      await this.showToast('Introduce el nombre de la categoría');
+      return;
+    }
+
+    if(this.currentCategory !== '' && this.process.currentRound.questions[this.questionIdx].categories.findIndex(c => c.catName.toLowerCase() == this.currentCategory.toLowerCase()) !== -1) {
+      await this.showToast('No puedes introducir categorías duplicadas');
+      this.currentCategory = '';
+      return;
+    }
+    this.process.currentRound.questions[this.questionIdx].categories.push(new Category(this.currentCategory));
+    this.currentCategory = '';
+    await this.httpClient.post(environment.apiUrl + '/v1/process/question/update?process_id=' + this.process.id,
+      this.process.currentRound.questions[this.questionIdx]).toPromise();
+    this.reorderCategories();
+  }
+  async delCategory(category: Category) {
+    this.process.currentRound.questions[this.questionIdx].categories = this.process.currentRound.questions[this.questionIdx].categories.filter((cat) => {
       return category.catName !== cat.catName;
     });
+    await this.httpClient.post(environment.apiUrl + '/v1/process/question/update?process_id=' + this.process.id,
+      this.process.currentRound.questions[this.questionIdx]).toPromise();
+    this.reorderCategories();
+  }
+  private reorderCategories() {
+    this.process.currentRound.questions[this.questionIdx].categories.sort((n1, n2) => {
+      if (n1.id < n2.id) {
+        return -1;
+      }
+      if (n1.id > n2.id) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  private async showToast(msg: string) {
+    const toast = await this.toastController.create({
+      position: 'top',
+      message: msg,
+    });
+    await toast.present();
+    setTimeout(() => {
+      toast.dismiss();
+    }, 3000);
+    return toast;
   }
 }
