a868c093b3993f914da6d8e677dad12cfcc6b725
andreigarcia <andreigarciacuadra@gmail.com>
2021-12-03 21:57:27 +0100

    eliminados avatares añadidas imagenes automaticas

--- src/app/logged-in/processes/processes.module.ts
+++ src/app/logged-in/processes/processes.module.ts
@@ -1,4 +1,4 @@
-import {IonicModule} from '@ionic/angular';
+import {IonAvatar, IonicModule} from '@ionic/angular';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -36,7 +36,7 @@ import {RemainingExpertsPage} from './single/questions/remaining_experts/remaini
     IonicRatingModule,
     CountdownModule,
     TranslateModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   declarations: [
     ProcessesPage,
